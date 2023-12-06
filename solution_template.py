16f2bd8b9faa195526cd5346a3aaa18486aacc2a
Kevin Buchik <kdbuchik@gmail.com>
2023-12-05 22:34:30 -0800

    Repository initialization; added solution template file

---
+++ solution_template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+## Advent of Code 2023
+## Day N - Puzzle Title
+## https://adventofcode.com/2023/day/n
+
+import os
+import sys
+
+with open("inputXX.txt") as f:
+    for line in f:
+        line = line.rstrip()
+
+print("Part One:")
+#print(str(part1) + "\n")
+#print("Part Two:")
+#print(str(part2) + "\n")
+exit(0)
